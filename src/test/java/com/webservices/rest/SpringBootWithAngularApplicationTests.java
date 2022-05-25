47c813cf7425bc4022a862f09adcf471e3e42a36
PijushKN <sagar.pknag@gmail.com>
2022-05-25 12:45:57 +0530

    Initial Commit

---
+++ src/test/java/com/webservices/rest/SpringBootWithAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webservices.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
