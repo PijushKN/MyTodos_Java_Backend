1e87a29ff9a0b58232870b40390289c3b14d2d3f
PijushKN <sagar.pknag@gmail.com>
2022-05-26 00:13:41 +0530

    Added First Rest Controller

--- src/main/java/com/webservices/rest/controller/HelloWorldController.java
+++ src/main/java/com/webservices/rest/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.webservices.rest.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 import com.webservices.rest.model.HelloWorldBean;
 
 @RestController
+@CrossOrigin(origins="http://localhost:4200")
 public class HelloWorldController {
 	
 	@GetMapping("/hello-world/{name}")
 	public HelloWorldBean showMessage(@PathVariable String name) {
 		return new HelloWorldBean("Hi, "+name+" welcome to our site");
+	}
+	
+	@GetMapping("/hello-world-bean")
+	public HelloWorldBean showMessage2() {
+		return new HelloWorldBean("Hi, welcome to our site");
 	}
 }
