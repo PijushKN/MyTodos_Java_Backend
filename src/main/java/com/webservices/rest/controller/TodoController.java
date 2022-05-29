6f5acd02b6c95bad53d0485b1b647bcb2bdac412
PijushKN <sagar.pknag@gmail.com>
2022-05-29 20:54:06 +0530

    PUT POST rest API created

--- src/main/java/com/webservices/rest/controller/TodoController.java
+++ src/main/java/com/webservices/rest/controller/TodoController.java
@@ -1,14 +1,20 @@
 package com.webservices.rest.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.webservices.rest.model.HelloWorldBean;
 import com.webservices.rest.model.Todo;
@@ -16,7 +22,7 @@ import com.webservices.rest.services.TodoService;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
-public class HelloWorldController {
+public class TodoController {
 
 	@Autowired
 	TodoService todoService;
@@ -34,6 +40,23 @@ public class HelloWorldController {
 	@GetMapping("/todos")
 	public List<Todo> getAllTodos() {
 		return todoService.retrieveAllTodos();
+	}
+	
+	@GetMapping("/todos/{id}")
+	public Todo retrieveTodo(@PathVariable int id) {
+		return todoService.retrieveTodoService(id);
+	}
+	
+	@PutMapping("/todos/{id}")
+	public ResponseEntity<Todo> updateTodo(@PathVariable int id,@RequestBody Todo todo){
+		Todo updatedTodo = todoService.save(todo);
+		return new ResponseEntity<Todo>(updatedTodo,HttpStatus.OK);
+	}
+	@PostMapping("/todos")
+	public ResponseEntity<Void> createTodo(@RequestBody Todo todo){
+		Todo createdTodo = todoService.save(todo);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(createdTodo.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 	@DeleteMapping("/todos/{id}")
--- src/main/java/com/webservices/rest/services/TodoService.java
+++ src/main/java/com/webservices/rest/services/TodoService.java
@@ -12,11 +12,34 @@ import com.webservices.rest.model.Todo;
 public class TodoService {
 
 	private static List<Todo> todos = new ArrayList<>();
+	private static int idCounter = 4;
 	static {
 		todos.add(new Todo(1, "Study Angular 12", new Date(), false));
 		todos.add(new Todo(2, "Study Spring Boot", new Date(), false));
 		todos.add(new Todo(3, "Study Selenium", new Date(), false));
 		todos.add(new Todo(4, "Study React", new Date(), false));
+	}
+	
+	public Todo save(Todo todo) {
+		if(todo.getId()==-1) {
+			todo.setId(++idCounter);
+			todos.add(todo);
+		}
+		else {
+			deleteTodoService(todo.getId());
+			todos.add(todo);
+		}
+		
+		return todo;
+	}
+	
+	public Todo retrieveTodoService(int id) {
+		for(Todo todo:todos) {
+			if(todo.getId()==id)
+				return todo;
+		}
+		
+		return null;
 	}
 
 	public List<Todo> retrieveAllTodos() {
